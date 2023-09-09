97e85206123cac866606659073650b03e6ed56ff
Tushar <tusharsolanki2001152@gmail.com>
2023-09-09 16:07:46 +0530

    Added AddNote component

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,16 +1,20 @@
 import React, {useContext} from 'react'
 import NoteContext from '../context/notes/noteContext';
 import {Noteitem} from './Noteitem'
+import { AddNote } from './AddNote' 
 
 export const Notes = () => {
     const context =  useContext(NoteContext);
-    const {notes, setNotes} = context;
+    const {notes, addNote} = context;
   return (
+    <>
+    <AddNote/>
     <div className="row my-3">
         <h2>You'r Notes</h2>
         {notes.map((note)=>{
           return <Noteitem key={note._id} note={note}/>
         })}
       </div>
+    </>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -60,8 +60,32 @@ const NoteState = (props) => {
       ]
       const [notes, setNotes] =  useState(notesInitial)
 
+      // Add a Note
+      const addNote = (title, description, tag) =>{
+        // TODO: API Call
+        console.log("Adding a new note")
+        const note = {"_id": "64f2eb960513ee4b9f010521",
+        "user": "64f0b08f2e962093baf09f4b",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "2023-09-02T08:00:22.307Z",
+        "__v": 0}
+        setNotes(notes.concat(note))
+      }
+
+      // Delete a Note
+      const deleteNote = () =>{
+        
+      }
+
+      // Edit a Note
+      const editNote = () =>{
+        
+      }
+
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
